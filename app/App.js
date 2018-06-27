2c1110c9a4fe827aa340df9aa1d44ba4d2c3cf0d
Jehan <jehan.tremback@gmail.com>
2018-06-26 17:09:46 -0700

    got form manager to work

--- app/App.js
+++ app/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { AragonApp, Button, Text, observe } from "@aragon/ui";
-import Aragon, { providers } from "@aragon/client";
+import { AragonApp, Text, observe, TextInput } from "@aragon/ui";
+// import Aragon, { providers } from "@aragon/client";
 import styled from "styled-components";
 
 const AppContainer = styled(AragonApp)`
@@ -9,15 +9,82 @@ const AppContainer = styled(AragonApp)`
   justify-content: center;
 `;
 
+class FormData {
+  constructor(validators, that) {
+    this.validators = validators;
+    this.that = that;
+  }
+
+  onFieldChange = e => {
+    const { name, value } = e.target;
+
+    this.that.setState({
+      fields: {
+        ...this.that.state.fields,
+        [name]: value
+      },
+
+      valid: {
+        ...this.that.state.valid,
+        [name]: this.validators[name] && this.validators[name](value)
+      }
+    });
+  };
+
+  isFieldValid = name => {
+    return this.that.state.fields && this.that.state.fields[name]
+      ? this.that.state.valid && this.that.state.valid[name]
+      : undefined;
+  };
+}
+
+class NewNodeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fields: {
+        eth_addr: "ope"
+      }
+    };
+    this.formData = new FormData(
+      {
+        eth_addr: value => {
+          return value.length > 8;
+        }
+      },
+      this
+    );
+  }
+
+  render() {
+    console.log("shit");
+    return (
+      <form onSubmit={this.onSubmit}>
+        fuck
+        <TextInput
+          name="eth_addr"
+          style={
+            this.formData.isFieldValid("eth_addr") === false
+              ? { background: "red" }
+              : {}
+          }
+          onChange={this.formData.onFieldChange}
+          value={this.state.fields.eth_addr || ""}
+          placeholder="Node Ethereum address"
+          required
+          wide
+        />
+      </form>
+    );
+  }
+}
+
 export default class App extends React.Component {
   render() {
     return (
       <AppContainer>
-        <div>
-          <ObservedCount observable={this.props.observable} />
-          <Button onClick={() => this.props.app.decrement(1)}>DERPP</Button>
-          <Button onClick={() => this.props.app.increment(1)}>Increment</Button>
-        </div>
+        <div>fuck that</div>
+        <NewNodeForm />
       </AppContainer>
     );
   }
