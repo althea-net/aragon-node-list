7a51e481d55df77e8b1a6b38f8086b3e72fbbe5a
Sebastian Bolanos <sebasheston@gmail.com>
2018-08-10 22:22:45 -0700

    Saving index verion of the contract.
    Mapping is better it seems

--- contracts/NodesList.sol
+++ contracts/NodesList.sol
@@ -6,21 +6,31 @@ import "@aragon/os/contracts/lib/zeppelin/math/SafeMath.sol";
 contract NodesList is AragonApp {
   using SafeMath for uint256;
 
-  event NewMember(address indexed ethNodeAddress, string ipAddress);
+  bytes4[] public ip;
+  address[] public addr;
+
+  event NewMember(address indexed ethNodeAddress, bytes4 ipAddress);
   event MemberRemoved(address indexed ethNodeAddress);
  
   bytes32 constant public ADD_MEMBER = keccak256("ADD_MEMBER");
   bytes32 constant public DELETE_MEMBER = keccak256("DELETE_MEMBER");
-
-  mapping(address => string) public nodesList;
-
-  function addMember(address ethAddr, string ip) auth(ADD_MEMBER) external {
-    nodesList[ethAddr] = ip;
-    NewMember(ethAddr, ip);
+  
+  function addMember(address _ethAddr, bytes4 _ip) auth(ADD_MEMBER) external {
+    require(addr.length<256);
+    addr.push(_ethAddr);
+    ip.push(_ip);
+    NewMember(_ethAddr, _ip);
   }
 
-  function deleteMember(address ethAddr) auth(DELETE_MEMBER) external {
-    nodesList[ethAddr] = '';
-    MemberRemoved(ethAddr);
+  function deleteMember(uint _index) auth(DELETE_MEMBER) external {
+    address _ethAddr = addr[_index];
+    delete(addr[addr.length]);
+    delete(ip[ip.length]);
+    MemberRemoved(_ethAddr);
+  }
+  
+  function getNodeList() public returns(address[] _addr, bytes4[] _ip) {
+      _addr = addr;
+      _ip = ip;
   }
 }
