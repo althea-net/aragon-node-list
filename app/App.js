494dc9aff318219eb50d649f358062da1575b64f
Sebastian Bolanos <sebasheston@gmail.com>
2018-08-14 00:14:29 -0700

    One needs <AragonApp> tag it seems

--- app/App.js
+++ app/App.js
@@ -5,7 +5,7 @@ import styled from "styled-components"
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
 import NewNodeForm from "./components/NewNodeForm"
-import NodesTables from "./components/NodesTable"
+import NodesTable from "./components/NodesTable"
 
 const AppContainer = styled(AragonApp)`
   display: flex;
@@ -15,22 +15,19 @@ const AppContainer = styled(AragonApp)`
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <AppContainer className="app">
+      <AragonApp className="app">
+        <AppContainer>
           <Col top>
+            <Row><NewNodeForm app={this.props.app} /></Row>
             <Row>
-              <NewNodeForm 
-                app={this.props.app}
-              />
-            </Row>
-            <Row>
-              <NodesTables 
-                app={this.props.app}
-              />
+              <ObservedNodesTable observable={this.props.observable} />
             </Row>
           </Col>
         </AppContainer>
-      </div>
+      </AragonApp>
     )
   }
 }
+
+const ObservedNodesTable = observe((state$) => state$, { nodes: [] })
+(NodesTable)
