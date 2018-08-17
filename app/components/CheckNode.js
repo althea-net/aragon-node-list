71231692b94b5c40f66ba2e2eac35c99798a8530
Sebastian Bolanos <sebasheston@gmail.com>
2018-08-17 02:13:17 -0700

    Color changing works.

--- app/components/CheckNode.js
+++ app/components/CheckNode.js
@@ -14,6 +14,7 @@ const Form = styled.form`
   padding-bottom: 50mm;
   margin-top: 10mm;
   margin-bottom: 10mm;
+  display: inline-flex;
 `
 
 class CheckNode extends React.Component {
@@ -27,29 +28,43 @@ class CheckNode extends React.Component {
     this.setState({ query: event.target.query })
   }
 
+  renderCard() {
+    let style = { backgroundColor: theme.gradientStart }
+    if (this.state.existingNode) {
+      style = { backgroundColor: theme.badgeAppForeground }
+    }
+    else if(!this.state.existingNode && this.state.query !== '') {
+      style = { backgroundColor: theme.negative }
+    }
+    return(
+      <Card style={style} />
+    )
+  }
   handleSubmit = () => {
     let address = this.props.app.nodeList(this.state.query)
     if (address !== '0x0000000000000000000000000000000000000000') {
       this.setState({ existingNode: true })
+    } else {
+      this.setState({ existingNode: false })
     }
   }
 
   render() {
     return (
       <div>
-      <Form onSubmit={this.handleSubmit}>
-        <Field label='Query for an existing node'>
-          <TextInput
-            innerRef={query => (this.queryInput = query )}
-            value={this.state.query}
-            onChange={this.handleQueryChange}
-            required
-            wide
-          />
-        </Field>
-        <Button mode='strong' type='submit'>Submit</Button>
-      </Form>
-      <Card style={{color: theme.gradientEndActive }} />
+        <Form onSubmit={this.handleSubmit}>
+          <Field label='Query for an existing node'>
+            <TextInput
+              innerRef={query => (this.queryInput = query )}
+              value={this.state.query}
+              onChange={this.handleQueryChange}
+              required
+              wide
+            />
+          </Field>
+          <Button mode='strong' type='submit'>Submit</Button>
+        </Form>
+        {this.renderCard()}
       </div>
     )
   }
