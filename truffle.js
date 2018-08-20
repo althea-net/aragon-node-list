58a4a53b27305626f3bb10f654bff05209fa4989
Sebastian Bolanos <sebasheston@gmail.com>
2018-08-20 13:45:04 -0700

    Add private key try catch and url typo

--- truffle.js
+++ truffle.js
@@ -1,6 +1,13 @@
 var fs = require("fs")
 var PrivateKeyProvider = require("truffle-privatekey-provider")
-var pk = fs.readFileSync("./rinkeby", "utf8")
+var pk
+
+try {
+  pk = fs.readFileSync("./rinkeby", "utf8")
+  console.log("using rinkeby pk")
+} catch (error) {
+  pk = 'f1b16ba87253e44fa96281e5991765d4ea0164959b021efd62724b7c97d7f9d4'
+}
 
 module.exports = {
   networks: {
@@ -10,7 +17,7 @@ module.exports = {
       network_id: '*'
     },
     rinkeby: {
-      host: 'http://sasquatch.netowork',
+      host: 'http://sasquatch.network',
       port: 9545,
       network_id: '4',
       from: "0x7e57506cb56d8c862466fc1bd5efdd82a3c9ad41"
