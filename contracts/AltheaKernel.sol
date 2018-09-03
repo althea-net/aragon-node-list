14cb191fda687ac455f8873bb365815c5a0158d0
Sebastian Bolanos <sebasheston@gmail.com>
2018-09-03 01:20:15 -0700

    Add contracts as templates of what I want to do

---
+++ contracts/AltheaKernel.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.18;
+
+import "@aragon/os/contracts/kernel/Kernel.sol";
+
+contract AltheaKernel is Kernel {
+
+  acl = ACL(kernel.acl())
+  acl.createPermission(address entity, address app, bytes32 role, address manager)
+  function addMemberTo() {
+  }
+
+}
