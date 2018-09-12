1b1a4c03b0861aff2342b9c92cc2a6e63f97d5a6
Sebastian Bolanos <sebasheston@gmail.com>
2018-09-11 17:48:25 -0700

    Combined contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
-var NodeList = artifacts.require('./NodeList.sol')
-var RenewalFeeEscrow = artifacts.require('./RenewalFeeEscrow.sol')
-var SubnetController = artifacts.require('./SubnetController.sol')
+var Althea = artifacts.require('./Althea.sol')
 
 module.exports = function (deployer, network, accounts) {
-  deployer.deploy(NodeList)
-  deployer.deploy(RenewalFeeEscrow, accounts[9])
-  deployer.deploy(SubnetController)
+  deployer.deploy(Althea)
 }
