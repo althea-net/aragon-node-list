fdaba34a1b0cc97c56551f42692d3a42905e319f
Adam Soltys <asoltys@gmail.com>
2018-09-12 13:40:21 -0700

    truncate ip and address, color funds red or green

--- app/components/NodeList.js
+++ app/components/NodeList.js
@@ -3,6 +3,8 @@ import { Button, Table, TableHeader, TableRow, TableCell, Text } from '@aragon/u
 import data from '../MockData'
 
 export default () => {
+  let fundsColor = funds => (funds > 0) ? "green" : "red"
+
   return (
     <Table
       header={
@@ -17,13 +19,17 @@ export default () => {
     >
       {data.map((d, i) => {
         let {nickname, funds, address, ip} = d;
+        let trunc = (s, n) => `${s.substr(0,n)}...${s.substr(-n)}`
+        address = trunc(address, 6)
+        ip = trunc(ip, 4)
+
         return (
           <TableRow key={i}>
             <TableCell>
               <Text>{nickname}</Text>
             </TableCell>
             <TableCell>
-              <Text>{funds}</Text>
+              <Text color={fundsColor(funds)}>{funds}</Text>
             </TableCell>
             <TableCell>
               <Text>{address}</Text>
