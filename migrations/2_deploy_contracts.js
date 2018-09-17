c3b2f20de43c0c21f5f3893fe75b01aa081926e3
Sebastian Bolanos <sebasheston@gmail.com>
2018-09-17 10:43:44 -0700

    Fix migration script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,5 @@
 var Althea = artifacts.require('./Althea.sol')
 
 module.exports = async (deployer, network, accounts) => {
-  paymentAddress = await web3.eth.personal.newAccount()
-  deployer.deploy(Althea, paymentAddress)
+  deployer.deploy(Althea)
 }
