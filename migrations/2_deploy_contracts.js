8992fb669111f98b444dd31887df10931cf6cd8b
Sebastian Bolanos <sebasheston@gmail.com>
2018-09-24 23:45:10 -0700

    Saving working deploy method that uses .new()

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 var Althea = artifacts.require('./Althea.sol')
 
 module.exports = async (deployer, network, accounts) => {
-  deployer.deploy(Althea)
+  const receipt = await Althea.new()
+  console.log("Althea contract: ", receipt.address)
 }
