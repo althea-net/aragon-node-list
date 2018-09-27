6c217b97e598a58d2495b2552c1a39d22804a14e
Sebastian Bolanos <sebasheston@gmail.com>
2018-09-26 21:01:33 -0700

    Change migrate function to deploy script

--- scripts/deploy-dao.js
+++ scripts/deploy-dao.js
@@ -1,5 +1,3 @@
-const apmMigration = require('@aragon/os/scripts/deploy-beta-apm.js')
-const daoFactoryMigration = require('@aragon/os/scripts/deploy-daofactory')
 const ipfsHashes = require('./ipfs.js')
 
 const financeIpfs = ipfsHashes.finance
@@ -8,34 +6,38 @@ const vaultIpfs = ipfsHashes.vault
 const votingIpfs = ipfsHashes.voting
 const altheaIpfs = ipfsHashes.althea
 
-module.exports = async (deployer, network, accounts, arts = null) => {
-  if (arts != null) artifacts = arts // allow running outside 
+module.exports = async (
+  truffleExecCallback,
+  {artifacts = this.artifacts, verbose = true} = {}
+) => {
+
+  const log = (...args) => { if (verbose) console.log(...args) }
+
   const Finance = artifacts.require('@aragon/apps-finance/contracts/Finance.sol')
   const TokenManager = artifacts.require('@aragon/apps-token-manager/contracts/TokenManager.sol')
   const Vault = artifacts.require('@aragon/apps-vault/contracts/Vault.sol')
   const Voting = artifacts.require('@aragon/apps-voting/contracts/Voting.sol')
-
-  const MiniMeTokenFactory = artifacts.require(
-    '@aragon/apps-shared-minime/contracts/MiniMeToken.sol'
-  )
-
+  const TokenFactory = artifacts.require('@aragon/apps-shared-minime/contracts/MiniMeToken.sol')
   const Althea = artifacts.require('./Althea.sol')
   const AltheaDAOFactory = artifacts.require('./AltheaDAOFactory.sol')
 
-
   console.log('naaah')
-  const { apm, ens } = await apmMigration(deployer, network, accounts, artifacts)
-  const { daoFactory } = await daoFactoryMigration(deployer, network, accounts, artifacts)
+
+  /*
+  const { apm, ens } = await apmMigration()
+  const { daoFactory } = await daoFactoryMigration()
+  */
   console.log('heeeyoo')
+
   const financeBase = await Finance.new()
   const tokenManagerBase = await TokenManager.new()
   const vaultBase = await Vault.new()
   const votingBase = await Voting.new()
   const altheaBase = await Althea.new()
+  const minimeFac = await TokenFactory.new()
+  console.log("MIME", minimeFac)
 
-
-  const minimeFac = await MiniMeTokenFactory.new()
-  const template = await AltheaDAO.new(daoFact.address, minimeFac.address, apm.address)
+  const template = await AltheaDAO.new(daoFactory.address, minimeFac.address, apm.address)
   await template.apmInit(
     financeBase.address,
     financeIpfs,
@@ -50,7 +52,6 @@ module.exports = async (deployer, network, accounts, arts = null) => {
   )
 
   const receipt = await template.createInstance()
-
   const daoAddr = receipt.logs.filter(l => l.event == 'DeployInstance')[0].args.dao
 
   const financeId = await template.financeAppId()
@@ -72,12 +73,10 @@ module.exports = async (deployer, network, accounts, arts = null) => {
   console.log("DAO's voting app:", votingAddr)
   console.log('ENS:', ensAddr)
 
-  return {
-    daoAddr,
-    ensAddr,
-    financeAddr,
-    tokenManagerAddr,
-    vaultAddr,
-    votingAddr,
+
+  if (typeof truffleExecCallback === 'function') {
+    truffleExecCallback()
+  } else {
+    return { daoAddr }
   }
 }
