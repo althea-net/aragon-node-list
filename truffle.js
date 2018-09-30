5b3253e3c5bcc0ffcaa733fb682b8cb306e5b7d8
Sebastian Bolanos <sebasheston@gmail.com>
2018-09-29 20:36:36 -0700

    Changed aragon os package to personal repo.
    This also changed truffle config. Now it impoorts the truffle
    config of aragonOS. Makes mantainning networks easier.

--- truffle.js
+++ truffle.js
@@ -1,5 +1,3 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-
 module.exports = {
   compilers: {
     solc: {
@@ -7,44 +5,5 @@ module.exports = {
       docker: true
     }
   },
-  networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*'
-    },
-    devnet: {
-      host: 'localhost',
-      port: 8535,
-      network_id: '*'
-    },
-    sasquatch_rpc: {
-      host: 'sasquatch.network',
-      port: 19545,
-      network_id: '4'
-    },
-    sasquatch_ws: {
-      host: 'sasquatch.network',
-      port: 19546,
-      network_id: '4'
-    },
-    rinkeby: {
-      provider: function() {
-        return new HDWalletProvider(
-          process.env.MNEMONIC,
-          "http://sasquatch.network:9545"
-        )
-      },
-      network_id: '4'
-    },
-    infura: {
-      provider: function() {
-        return new HDWalletProvider(
-          process.env.MNEMONIC,
-          "https://rinkeby.infura.io/" + process.env.INFURA_API
-        )
-      },
-      network_id: '4'
-    }
-  }
+  networks: require("@aragon/os/truffle-config.js").networks
 }
