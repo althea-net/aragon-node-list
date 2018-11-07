45a192e4f918ee0dbc69c77712a39f26baace12d
Sebastian Bolanos <sebasheston@gmail.com>
2018-11-07 21:48:31 +0100

    Changed enviroment variables and addresses

--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   compilers: {
     solc: {
@@ -8,12 +10,12 @@ module.exports = {
   networks: {
     development: {
       host: 'localhost',
-      port: 8535,
+      port: 8545,
       network_id: '*'
     },
     sasquatch_rpc: {
       host: 'sasquatch.network',
-      port: 9545,
+      port: 19545,
       network_id: '4'
     },
     sasquatch_ws: {
@@ -21,6 +23,23 @@ module.exports = {
       port: 19546,
       network_id: '4'
     },
-    ...require("@aragon/os/truffle-config.js").networks
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "http://sasquatch.network:9545"
+        )
+      },
+      network_id: '4'
+    },
+    infura: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "https://rinkeby.infura.io/" + process.env.INFURA_API
+        )
+      },
+      network_id: '4'
+    }
   }
 }
