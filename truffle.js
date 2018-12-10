20290f11b54be36de502ba76e7603658e8ae4c28
Sebastian Bolanos <sebasheston@gmail.com>
2018-12-10 16:29:27 +0000

    Revert truffle-js config to function

--- truffle.js
+++ truffle.js
@@ -14,10 +14,11 @@ module.exports = {
       network_id: '*'
     },
     rinkeby2: {
-      provider: new HDWalletProvider(
-         process.env.MNEMONIC,
-         "http://sasquatch.network:19545"
-      ),
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          "http://sasquatch.network:19545"
+      )},
       network_id: '4',
       gas: 6.9e5,
       gasPrice: 150000000
