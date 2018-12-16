9619487f1a26960964838c9f388cef854a02a030
Sebastian Bolanos <sebasheston@gmail.com>
2018-12-16 16:32:20 +0000

    Corrected getVaultAddress...
    Should have reacd in more detail the spec

--- scripts/getVaultAddress.js
+++ scripts/getVaultAddress.js
@@ -3,16 +3,16 @@ const kernelABI = require('../build/contracts/Kernel.json').abi
 const namehash = require('eth-ens-namehash').hash
 
 const w3 = new Web3('https://sasquatch.network/rinkeby')
+
 const address = '0xBF6879f5f9AdD7D36b5c5B4f63E59fd4E32A6A1c'
+
 log = console.log
 
 async function main() {
   log('connected ', await w3.eth.getBlockNumber())
   const DAO = await new w3.eth.Contract(kernelABI, address)
-  let vaultId = namehash('vault.aragonpm.eth')
-  let namespace = await DAO.methods.APP_BASES_NAMESPACE().call()
-  let result = await DAO.methods.getApp(namespace, vaultId).call()
-  console.log('Vault: ', result)
+  let result = await DAO.methods.getRecoveryVault().call()
+  log('Vault: ', result)
 }
 
 main()
