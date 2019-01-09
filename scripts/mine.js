72fec67fa4f19542012b8e62e6d92d6e133b613f
Sebastian Bolanos <sebasheston@gmail.com>
2019-01-09 17:54:17 +0100

    Remove ethers from mine script

--- scripts/mine.js
+++ scripts/mine.js
@@ -1,19 +1,22 @@
-const ethers = require('ethers')
+const Web3 = require('web3')
+const w3 = new Web3('http://localhost:8545')
 
-//new ethers . providers . JsonRpcProvider( [ urlOrInfo = “http://localhost:8545” ] [ , network ] )
-const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
-
-async function mineBlock() {
-  console.log('hi')
-  await provider.send("evm_mine")
-}
-
-async function mineBlocks(count) {
+async function mineBlocks(count, cb) {
   let i = 0;
+  console.log('Start: ', await w3.eth.getBlockNumber())
   while (i < count) {
-    await mineBlock();
+    w3.currentProvider.send({
+      method:"evm_mine"
+    }, (error, result) => {
+      if(error) {
+        console.log('error', error)
+      }
+      return
+    })
     i++;
   }
+  console.log('End: ', await w3.eth.getBlockNumber())
+  console.log('Done')
 }
 
 mineBlocks(50)
