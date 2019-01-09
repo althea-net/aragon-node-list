28f4e9af5e360eea35f34e604f50de4292564d94
Sebastian Bolanos <sebasheston@gmail.com>
2019-01-09 14:13:29 +0100

    Convert node list to React component

--- app/components/NodeList.js
+++ app/components/NodeList.js
@@ -18,52 +18,61 @@ const RemoveButton = ({ app, ip, t }) => {
   return <Button emphasis="negative" onClick={deleteMember} mode="outline">{t("remove")}</Button>
 } 
 
-export default translate()(({ app, nodes, t }) => {
-  let fundsColor = funds => (funds > 0) ? "green" : "red"
-  if (!nodes || !nodes.length) return <Text>{t('noNodes')}</Text>
+class NodeList extends React.Component {
+  constructor(){
+    super()
+  }
 
-  return (
-    <div>
-      <Table
-        header={
-          <TableRow>
-            <TableHeader title={t('nickname')} />
-            <TableHeader title={t('fundsRemaining')} />
-            <TableHeader title={t('ethAddress')} />
-            <TableHeader title={t('ipAddress')} />
-            <TableHeader title={t('removeNode')} />
-          </TableRow>
-        }
-      >
-        {nodes.map((d, i) => {
-          let {nickname, bill, ethAddress, ipAddress} = d;
-          let trunc = (s, n) => `${s.substr(0,n)}...${s.substr(-n)}`
-          nickname = web3Utils.toUtf8(nickname)
-          let addr = Address6.fromBigInteger(new BigInteger(ipAddress.substr(2), 16))
-          let ip = addr.correctForm() + '/64'
+  fundsColor = funds => (funds > 0) ? "green" : "red"
 
-          let balance = web3Utils.fromWei(bill.balance.toString())
-          return (
-            <TableRow key={i}>
-              <TableCell>
-                <Text>{nickname}</Text>
-              </TableCell>
-              <TableCell>
-                <Text color={fundsColor(balance)}>&Xi;{balance}</Text>
-              </TableCell>
-              <TableCell>
-                <Text><Abbr title={ethAddress}>{trunc(ethAddress, 6)}</Abbr></Text>
-              </TableCell>
-              <TableCell>
-                <Text><Abbr>{ip}</Abbr></Text>
-              </TableCell>
-              <TableCell>
-                <RemoveButton app={app} ip={ipAddress} t={t} />
-              </TableCell>
+  render() {
+    let {nodes, t, app} = this.props
+    if (!nodes || !nodes.length) return <Text>{t('noNodes')}</Text>
+    return (
+      <div>
+        <Table
+          header={
+            <TableRow>
+              <TableHeader title={t('nickname')} />
+              <TableHeader title={t('fundsRemaining')} />
+              <TableHeader title={t('ethAddress')} />
+              <TableHeader title={t('ipAddress')} />
+              <TableHeader title={t('removeNode')} />
             </TableRow>
+          }
+        >
+          {nodes.map((d, i) => {
+            let {nickname, bill, ethAddress, ipAddress} = d;
+            let trunc = (s, n) => `${s.substr(0,n)}...${s.substr(-n)}`
+            nickname = web3Utils.toUtf8(nickname)
+            let addr = Address6.fromBigInteger(new BigInteger(ipAddress.substr(2), 16))
+            let ip = addr.correctForm() + '/64'
+            console.log('HEYO', app)
+
+            let balance = web3Utils.fromWei(bill.balance.toString())
+            return (
+              <TableRow key={i}>
+                <TableCell>
+                  <Text>{nickname}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text color={this.fundsColor(balance)}>&Xi;{balance}</Text>
+                </TableCell>
+                <TableCell>
+                  <Text><Abbr title={ethAddress}>{trunc(ethAddress, 6)}</Abbr></Text>
+                </TableCell>
+                <TableCell>
+                  <Text><Abbr>{ip}</Abbr></Text>
+                </TableCell>
+                <TableCell>
+                  <RemoveButton app={app} ip={ipAddress} t={t} />
+                </TableCell>
+              </TableRow>
+            )}
           )}
-        )}
-      </Table>
-    </div>
-  );
-}) 
+        </Table>
+      </div>
+    );
+  }
+}
+export default translate()(NodeList)
